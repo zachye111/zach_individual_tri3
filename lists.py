750d8b40f545ac94944f3bc222059e933e4ea4f4
zachye111 <37047405+zachye111@users.noreply.github.com>
2022-03-16 12:18:44 -0700

    lists

---
+++ lists.py
@@ -0,0 +1,114 @@
+# Hack 1: InfoDB lists.  Build your own/personalized InfoDb with a list length > 3,  create list within a list as illustrated with Owns_Cars
+
+InfoDb = []
+# List with dictionary records placed in a list
+InfoDb.append({
+    "FirstName": "John",
+    "LastName": "Mortensen",
+    "DOB": "October 21",
+    "Residence": "San Diego",
+    "Email": "jmortensen@powayusd.com",
+    "Owns_Cars": ["2015 Fusion", "2011 Ranger", "2003 Excursion", "1997 F-350", "1969 Cadillac"]
+})
+InfoDb.append({
+    "FirstName": "Chase",
+    "LastName": "Ott",
+    "DOB": "Decemeber 22",
+    "Residence": "San Diego",
+    "Email": "chaseo08899@stu.powayusd.com",
+    "Owns_Cars": ["Toyota Sienna 2005",]
+})
+
+
+InfoDb.append({
+    "FirstName": "Sunny",
+    "LastName": "Naidu",
+    "DOB": "August 2",
+    "Residence": "San Diego",
+    "Email": "snaidu@powayusd.com",
+    "Owns_Cars": ["A", "B", "C"]
+})
+
+
+# given an index this will print InfoDb content
+def print_data(n):
+    print(InfoDb[n]["FirstName"], InfoDb[n]["LastName"])  # using comma puts space between values
+    print("\t", "Cars: ", end="")  # \t is a tab indent, end="" make sure no return occurs
+    print(", ".join(InfoDb[n]["Owns_Cars"]))  # join allows printing a string list with separator
+    print()
+
+
+# Hack 2: InfoDB loops. Print values from the lists using three different ways: for, while, recursion
+## hack 2a: def for_loop()
+## hack 2b: def while_loop(0)
+## hack 2c : def recursive_loop(0)
+
+def tester():
+    print("For loop")
+    for_loop()
+    print("While loop")
+    while_loop(0)  # requires initial index to start while
+    print("Recursive loop")
+    recursive_loop(0)  # requires initial index to start recursion
+
+
+# for loop iterates on length of InfoDb
+def for_loop():
+    for n in range(len(InfoDb)):
+        print_data(n)
+
+
+# while loop contains an initial n and an index incrementing statement (n += 1)
+def while_loop(n):
+    while n < len(InfoDb):
+        print_data(n)
+        n += 1
+    return
+
+
+# recursion simulates loop incrementing on each call (n + 1) until exit condition is met
+def recursive_loop(n):
+    if n < len(InfoDb):
+        print_data(n)
+        recursive_loop(n + 1)
+    return  # exit condition
+
+
+# Factorial of a number using recursion
+def recur_factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * recur_factorial(n - 1)
+
+
+# this is test driver or code that plays when executed directly, versus import which will not run these statements
+def tester():
+    num = int(input("Enter a number for factorial: "))
+    # check if the number is negative
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    else:
+        print("The factorial of", num, "is", recur_factorial(num))
+
+# Hack 3: Fibonacci.  Write a recursive program to create a fibonacci sequence including error handling for invalid input
+# Function for nth Fibonacci number
+def Fibonacci(n):
+
+    # Check if input is 0 then it will
+    # print incorrect input
+    if n < 0:
+        print("Incorrect input")
+
+    # Check if n is 0
+    # then it will return 0
+    elif n == 0:
+        return 0
+
+    # Check if n is 1,2
+    # it will return 1
+    elif n == 1 or n == 2:
+        return 1
+
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
